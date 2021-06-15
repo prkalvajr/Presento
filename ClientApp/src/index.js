2f942e81d1be95d1eb4ec8487f95d2f118046b23
unknown <pedro.kalva@gmail.com>
2021-06-15 18:01:24 -0300

    Working log in component

--- ClientApp/src/index.js
+++ ClientApp/src/index.js
@@ -1,18 +1,22 @@
 import 'bootstrap/dist/css/bootstrap.css';
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { BrowserRouter } from 'react-router-dom';
 import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+import { Auth0Provider } from '@auth0/auth0-react';
 
-const baseUrl = document.getElementsByTagName('base')[0].getAttribute('href');
-const rootElement = document.getElementById('root');
+const domain = process.env.REACT_APP_AUTH0_DOMAIN;
+const clientId = process.env.REACT_APP_AUTH0_CLIENT_ID;
 
 ReactDOM.render(
-    <BrowserRouter basename={baseUrl}>
+    <Auth0Provider
+        domain="presento.us.auth0.com"
+        clientId="NfcXnFG5PlbOd0DNH94ilDDwllexyAAb"
+        redirectUri={window.location.origin}>
         <App />
-    </BrowserRouter>,
-    rootElement);
+    </Auth0Provider>,
+    document.getElementById('root')
+);
 
-registerServiceWorker();
 
+// {domain}
+// 
